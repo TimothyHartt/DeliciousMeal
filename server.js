d5d190f3cabaacbd26a47c75c0e1191d813de02b
danduggan98 <55000185+danduggan98@users.noreply.github.com>
2019-11-05 14:57:49 -0700

    Update server.js
    Added user sessions, database session store, 404 page, PUG integration, and search function template , plus minor bug fixes and improvements.

--- server.js
+++ server.js
@@ -1,43 +1,55 @@
 //Website server using Express
 
 //TODO
-    //Add 'username not found' and 'incorrect password' alerts
+    //Add 'username not found' and 'incorrect password' alerts using PUG
     //Ensure valid form inputs are kept when submitting unsuccessfully
-    //Add session cabability to maintain/track logins
-    //Let them log out
+    //Prevent new login attempts while already logged in (before they click the link, not after)
+    //Stop multiple people logging in with the same account
 
 //Grab packages
 const express = require('express');
 const mysql = require('mysql');
 const session = require('express-session');
-const bodyParser = require('body-parser');
-const security = require('./Backend-files/securityTools.js'); //Import our security tools for hashing
+const MySQLStore = require('express-mysql-session')(session); //Lets us store sessions in our database
+const bodyParser = require('body-parser'); //Lets us grab form inputs
+const security = require('./Backend-files/securityTools.js'); //Import our security tools
+const funcs = require('./Backend-files/serverFunctions.js'); //Import our external server functions
 
 //Set up express app
 var app = express();
 app.use(express.static(__dirname + '/')); //Use relative path names
-app.use(bodyParser.urlencoded({ extended: true })); //Body parser - lets us grab data from the form
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.set('view engine', 'pug'); //Use PUG for dynamic HTML
 
-app.use(session({ //Track sessions
-    secret: security.generateSalt(), //Special session identifier - our salt function creates a value that works for this
-    resave: false, //Don't force a save of the session info if nothing has changed
-    saveUninitialized: true //Save a user if it is their first time visiting the site
-}));
-
-//Set up database connection
-var connection = mysql.createConnection({
+//Set up database connection and MySQL Store
+var dbOptions = {
     host: 'localhost',
     user: 'root',
     password: '',
     database: 'deliciousmeal'
-});
+};
+
+var connection = mysql.createConnection(dbOptions);
+var sessionStore = new MySQLStore(dbOptions, connection);
+
+//Set up session tracking
+app.use(session({
+    secret: security.generateSalt(), //Special session identifier - our salt function creates a value that works for this
+    store: sessionStore, //Store the sessions in our database
+    resave: false, //Don't save session info if nothing has changed
+    saveUninitialized: true //Save a user if it's their first time visiting the site
+}));
 
 // ------ Display Web pages ------ //
 
 //Home Page
 app.get('/home', (req, res) => {
-    res.sendFile(__dirname + '/' + 'Frontend-files/home.html');
+    //Render the PUG page, passing in the session username
+    res.render('home', {
+        un: req.session.username
+    });
+    //res.sendFile(__dirname + '/' + 'Frontend-files/home.html');
 });
 
 //Login Page
@@ -60,45 +72,54 @@ app.post('/loginRequest', (req, res) => {
     var username = req.body.username;
     var password = req.body.password;
 
-    //Database query - sees if the given username exists and grabs their salt/hashed password data if they do
+    //Database query - Check username exists, grab salt and hashed password if it does
     var query = `SELECT salt, hashedPassword FROM users WHERE username = '${username}'`;
 
-    //Query the database if both fields submitted
-    if (username && password) {
-        connection.query(query, (error, results, fields) => {
-            //If username found
-            if (results.length > 0) {
-                //Put query results into a usable form
-                var userSalt = results[0].salt;
-                var userPW = results[0].hashedPassword;
+    //Check the user is not already logged in
+    if (!req.session.user) {
+        //Query the database if both fields submitted
+        if (username && password) {
+            connection.query(query, (err, results) => {
+                if (err) throw err;
 
-                //Hash the given password with the user's salt
-                var hash = security.SHA256(password + userSalt);
+                //If username found
+                if (results.length > 0) {
+                    //Put query results into a usable form
+                    var userSalt = results[0].salt;
+                    var userPW = results[0].hashedPassword;
 
-                //Compare computed hash to user's stored password hash
-                if (hash == userPW) {
-                    //If the passwords match, login and redirect to home page
-                    req.session.loggedin = true;
-                    req.session.username = username;
-                    res.redirect('/home');
-                    console.log(`- User '${username}' logged in successfully`);
-                }
-                else {
+                    //Hash the given password with the user's salt
+                    var hash = security.SHA256(password + userSalt);
+
+                    //If the hash matches the stored password, login and redirect to home page
+                    if (hash == userPW) {
+                        req.session.loggedin = true;
+                        req.session.username = username;
+                        res.redirect('/home');
+                        console.log(`- User '${username}' logged in successfully`);
+                    }
                     //If not, go back to the login page to try again
-                    res.redirect('/login');
-                    console.log(`- User '${username}' failed to login`);
+                    else {
+                        res.redirect('/login');
+                        console.log(`- User '${username}' failed to login`);
+                    }
                 }
-            }
-            //If username not found, go back to the login page
-            else {
-                console.log(`- Login attempt failed for username '${username}'`);
-                res.redirect('/login');
-            }
-        });
+                //If username not found, go back to the login page
+                else {
+                    console.log(`- Login attempt failed for username '${username}'`);
+                    res.redirect('/login');
+                }
+            });
+        }
+        //If at least one field left blank, have them try again
+        else {
+            res.redirect('/login');
+        }
     }
-    //If at least one field left blank, have them try again
+    //Redirect if they are already logged in
     else {
-        res.redirect('/login');
+        res.redirect('/home');
+        console.log(`- User ${req.session.username} already logged in`);
     }
 });
 
@@ -118,23 +139,24 @@ app.post('/createAccount', (req, res) => {
 
         //Check that the username does not already exist
         var checkQuery = `SELECT userID FROM users WHERE username = '${username}'`;
-        connection.query(checkQuery, (error, results, fields) => {
-            //If username found, go back to register page
+        connection.query(checkQuery, (err, results) => {
+            if (err) throw err;
+            
+            //If username already exists
             if (results.length > 0) {
                 res.redirect('/register');
             }
-            //If username is new and unique
             else {
+                //If username is new and both passwords match, hash and salt the password
                 if (password == confirmPassword) {
-                    //Hash and salt the user's password
                     var salt = security.generateSalt();
                     var hash = security.SHA256(password + salt);
 
                     //Add the new user to the database
                     var insertQuery = `INSERT INTO users (username, firstName, lastName, email, salt, hashedPassword)
                                        VALUES ('${username}', '${firstName}', '${lastName}', '${email}', '${salt}', '${hash}')`;
-                    connection.query(insertQuery, (error, result) => {
-                        if (error) throw error;
+                    connection.query(insertQuery, (err) => {
+                        if (err) throw err;
                         console.log(`- New user with username '${username}' successfully added to database`);
                         
                         //Log them in and redirect to homepage
@@ -152,7 +174,23 @@ app.post('/createAccount', (req, res) => {
         });
     }
 });
-// ------------------------------------------ //
+
+//Log the user out by deleting their current session
+app.post('/logout', (req, res) => {
+    console.log(`- Logged out user '${req.session.username}'`);
+    req.session.destroy();
+    res.redirect('/home');
+});
+
+app.post('/search', (req, res) => {
+    //INSERT SEARCH ALGORITHM HERE
+});
+// ------------------------------------------------------- //
+
+//Handle 404 errors
+app.use(function (res) {
+    res.status(404).send("404 - Page not found");
+})
 
 // Leave the NodeJS web server listening on port 5000
 var server = app.listen(5000, () => {
