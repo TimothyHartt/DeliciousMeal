df2255ae91af82f90bfe72f60a533c52dc272d4a
Asmita <32140160+asmita19@users.noreply.github.com>
2019-11-18 16:23:52 -0700

    Settings page

---
+++ Frontend-files/settings.js
@@ -0,0 +1,7 @@
+let container    = document.querySelector('.canvas')
+let toggleButton = document.querySelector('.toggle-button')
+
+toggleButton.addEventListener('click', (e) => {
+  e.preventDefault();
+  container.classList.toggle('show-nav');  
+});
