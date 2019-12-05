433e9deaa137333fb85c2b61bc2032e86cfe903c
Asmita <32140160+asmita19@users.noreply.github.com>
2019-12-04 17:07:14 -0700

    Delete settings.js

--- Frontend-files/settings.js
+++
@@ -1,7 +0,0 @@
-let container    = document.querySelector('.canvas')
-let toggleButton = document.querySelector('.toggle-button')
-
-toggleButton.addEventListener('click', (e) => {
-  e.preventDefault();
-  container.classList.toggle('show-nav');
-});
